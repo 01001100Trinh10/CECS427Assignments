40542512675d1be7695ca9f43cdbbe6033b2b208
01001100Trinh10 <luketrinh101@gmail.com>
2025-02-03 23:59:09 -0800

    Add bfs
    Adding basic bfs stuff. Nothing much yet and it doesn't work but I'll add more once I read more on how to implement BFS.

--- graph.py
+++ graph.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 # Creates random graph and save it as a gml
 def create_random_graph(n, c, gml):
-    # Given equation for the probability of generation of nodes (clnn)/n where n is the number of nodes
+    # Given equation for the probability of generation of nodes ((c)ln(n))/(n) where n is the number of nodes
     p = c * np.log(n)
     p = p / n
     graph = nx.erdos_renyi_graph(n, p)
@@ -12,4 +12,14 @@ def create_random_graph(n, c, gml):
         nx.write_gml(graph, gml)
     print(f"Graph with {n} nodes created and saved to {gml}.")
 
+
+# Read gml then use BFS to form a tree
+def bfs(gml, start):
+    # Reads gml file
+    graph = nx.read_gml(gml)
+
+    # Performs bfs on the graph
+    bfsTree = nx.bfs_tree(graph, source=start)
     
+    plt.figure(figsize=(10, 8))
+    plt.title('BFS Tree')
